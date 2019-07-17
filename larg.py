681d9848c55f0a2567a02a54e8299460d1363405
prudhvireddy321 <40226166+prudhvireddy321@users.noreply.github.com>
2019-07-17 09:34:31 +0530

    Update larg.py

--- larg.py
+++ larg.py
@@ -1,8 +1,7 @@
-    
-u,h=input().split()
-w=abs(len(h)-len(u))
+u,v=input().split()
+w=abs(len(v)-len(u))
 for i in range(len(u)):
-    if(len(h)==1 and v[i] in u):
+    if(len(v)==1 and v[i] in u):
         break
     if(u[i]!=v[i]):
         w=w+1
